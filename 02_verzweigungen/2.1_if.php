2855af66d40f9ca85572e8ce40e25f31106a8cdf
zauner-martin <zauner.martin1980@gmail.com>
2018-02-16 18:36:22 +0100

    new files

---
+++ 02_verzweigungen/2.1_if.php
@@ -0,0 +1,14 @@
+<?php
+
+$vorname = "";
+if ($vorname == "Donald") {
+  echo "Mögliche Berufe: US-Präsident oder Disney-Figur";
+}
+elseif ($vorname == "Kim") {
+  echo "Mögliche Berufe: Parteivorsitzender oder Supermodel";
+}
+else () {
+  echo "Mögliche Berufe: Alle Berufe mit weniger Publicity";
+}
+
+?>
